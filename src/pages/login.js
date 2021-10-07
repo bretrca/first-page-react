ac24d7cf93e36f6f179c1ca2ab91ab750a8458b1
Brayan Trujillo <briann320@gmail.com>
2021-10-07 09:36:14 +0200

    styles

---
+++ src/pages/login.js
@@ -0,0 +1,46 @@
+import React, {useState} from "react";
+import "../css/login.sass";
+
+function Login() {
+  const [isRegistring, setIsRegistring] = useState(true);
+
+  return (
+    <div className="Form-container">
+      {isRegistring ? (
+        <>
+          <h2> Please log in</h2>
+          <form className="Form-element" /*onSubmit={handleSubmit}*/>
+            <label>User</label>
+            <input
+              type="text"
+             /* onChange={(e) => setUsername(e.target.value)}*/
+              placeholder="username or email"
+            />
+            <label>Password</label>
+            <input
+              type="password"
+             /* onChange={(e) => setPassword(e.target.value)}*/
+              placeholder="password"
+            />
+            <button type="submit" /*onSubmit={handleSubmit}*/>
+              Enter
+            </button>
+          </form>
+          <div className="register-element">
+            <p /*onClick={toggleRegister}*/>
+              {" "}
+              Do yo want to join us ? <a href="/register">Register</a>
+            </p>
+          </div>
+        </>
+      ) : (
+        <>
+          <h2> or register</h2>
+          {/*  <Register toggleRegister={toggleRegister} />*/}
+        </>
+      )}
+    </div>
+  );
+}
+
+export default Login;
